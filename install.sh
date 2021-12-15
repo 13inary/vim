082b780a5ff3f3e0e24f1b612cba9bb1f4939ddb
keysquivered <936095897@qq.com>
2021-12-15 20:52:06 +0800

    add : curl vimConfig

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # neovim
 #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo ~/.vim/vimConfig --create-dirs \
+    https://raw.githubusercontent.com/keysquivered/vim/master/vimConfig
