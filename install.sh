6b8dd7b11e0b8ca292c582d3510c454d54b9c196
keysquivered <936095897@qq.com>
2021-12-15 21:00:20 +0800

    add go.snippets, coc-settings

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 # vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # neovim
 #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo ~/.vim/UltiSnips/go.snippets --create-dirs \
+    https://raw.githubusercontent.com/keysquivered/vim/master/UltiSnips/go.snippets
 curl -fLo ~/.vim/vimConfig --create-dirs \
     https://raw.githubusercontent.com/keysquivered/vim/master/vimConfig
+curl -fLo ~/.vim/coc-settings.json --create-dirs \
+    https://raw.githubusercontent.com/keysquivered/vim/master/coc-settings.json
