fbc984f006043e362bdbdc6727528e54e941c0a8
keysquivered <936095897@qq.com>
2021-12-15 21:23:55 +0800

    change vimConfig to vimConfig.vim

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
        #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 curl -fLo ~/.vim/UltiSnips/go.snippets --create-dirs \
     https://raw.githubusercontent.com/keysquivered/vim/master/UltiSnips/go.snippets
-curl -fLo ~/.vim/vimConfig --create-dirs \
-    https://raw.githubusercontent.com/keysquivered/vim/master/vimConfig
+curl -fLo ~/.vim/vimConfig.vim --create-dirs \
+    https://raw.githubusercontent.com/keysquivered/vim/master/vimConfig.vim
 curl -fLo ~/.vim/coc-settings.json --create-dirs \
     https://raw.githubusercontent.com/keysquivered/vim/master/coc-settings.json
